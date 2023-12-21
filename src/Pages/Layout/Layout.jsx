d7f311cd68e683bae5929dedf89f7c4003950c2f
swamithedev <swamithedev@gmail.com>
2023-12-21 14:36:07 +0530

    set the layout ui

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,11 +1,17 @@
+import RandomImage from "../RandomImage/RandomImage"
+import Footer from "../footer/Footer"
+
 function Layout() {
     return (
         <>
-            <div style={{ height: '90vh' }}>
+            <div className="container">
 
+                <div style={{ height: '90vh' }}>
+                    <RandomImage />
+                </div>
             </div>
             <div style={{ height: '10vh' }}>
-
+                <Footer />
             </div>
         </>
     )
