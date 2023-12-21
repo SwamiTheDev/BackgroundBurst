8d9aa2d2f0e680787ae199cedfe374d97d4e09d5
swamithedev <swamithedev@gmail.com>
2023-12-21 14:35:39 +0530

    update a footer component

---
+++ src/Pages/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+export default function Footer() {
+    return (
+
+        <>
+            <footer>
+                Desgin and Developed by <a href="https://swamithedev.vercel.app">Swamithedev</a>
+            </footer>
+        </>
+    )
+}
