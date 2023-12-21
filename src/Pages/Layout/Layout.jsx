2363635de4a19badff64be54ee0ced654b8e4322
swamithedev <swamithedev@gmail.com>
2023-12-21 13:34:28 +0530

    add the folder structure

---
+++ src/Pages/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+function Layout() {
+    return (
+        <>
+            <div style={{ height: '90vh' }}>
+
+            </div>
+            <div style={{ height: '10vh' }}>
+
+            </div>
+        </>
+    )
+}
+
+export default Layout
